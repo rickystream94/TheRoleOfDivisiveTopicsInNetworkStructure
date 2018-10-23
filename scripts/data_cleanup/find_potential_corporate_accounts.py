4ab57e1464f0fd647f9159c1ae00001ca1e68629
Riccardo Cannistrà <rickystream94@gmail.com>
2018-10-23 18:44:14 +0200

    Identified issue with cleanup, fixing afterwards

--- scripts/data_cleanup/find_potential_corporate_accounts.py
+++ scripts/data_cleanup/find_potential_corporate_accounts.py
@@ -22,12 +22,12 @@ def read_large_file(file_object, start_from_line):
             yield chunk
             chunk = []
 
-def get_verified_accounts(start_from_line=0):
+def find_potential_corporate_accounts(start_from_line=0):
     companies_filename = "../../data/found_companies.csv"
     output_filename = "../../data/potential_companies.csv"
     
     # Setup tweepy API
-    with open("../lib/GetOldTweets-python/twitter_credentials.json") as credentials_file:
+    with open("../../lib/GetOldTweets-python/twitter_credentials.json") as credentials_file:
         credentials = json.load(credentials_file)
 
     auth = tweepy.AppAuthHandler(credentials["consumer_key"], credentials["consumer_secret"])
@@ -45,16 +45,41 @@ def get_verified_accounts(start_from_line=0):
                         users = api.lookup_users(screen_names=[el.split(',')[0] for el in chunk])
                         success = True
                     except tweepy.TweepError as ex:
+                        if ex.api_code == 17:
+                            for el in chunk:
+                                out_f.write(",".join([el,str(False),str(0)]))
+                            continue
                         print("Twitter error: '{0}', sleeping for 300 seconds...".format(ex.args[0][0]['message']))
                         time.sleep(300)
-                verified_lines = [el for el in chunk for user in users if el.split(',')[0] == user._json["screen_name"].lower() and user._json["verified"]]
-                for verified_line in verified_lines:
-                    out_f.write(verified_line + "\n")
+                for el in chunk:
+                    username, encoding = el.split(',')
+                    for user in users:
+                        if username == user._json["screen_name"].lower():
+                            is_user_verified = user._json["verified"]
+                            user_description = user._json["description"]
+                            num_matching_keywords = get_num_matching_keywords(user_description)
+                            out_f.write(",".join([el,str(is_user_verified),str(num_matching_keywords)]) + "\n")
                 count +=len(chunk)
-                if count % 5000000 == 0:
+                if count % 5000 == 0:
                     print("Currently processed {0} usernames...".format(count))
     print("Done! Processed {0} usernames.".format(count))
 
+def get_num_matching_keywords(description):
+    if description is None:
+        return 0
+    keywords = ["customer","customers","support","service","help","ask",
+                "team","care","information","helpteam","questions",
+                "concerns","inquiries","assistance","assist","answers","queries","official"]
+    desc_words = [word.lower() for word in description.split()]
+    matching_keywords = 0
+    for keyword in keywords:
+        if keyword in desc_words:
+            matching_keywords += 1
+    return matching_keywords
+    
 if __name__ == '__main__':
-    start_from_line = sys.argv[1]
-    get_verified_accounts(int(start_from_line))
+    if len(sys.argv) == 2:
+        start_from_line = sys.argv[1]
+    else:
+        start_from_line = 0
+    find_potential_corporate_accounts(int(start_from_line))
