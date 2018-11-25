dfdd9d8cd148cdbcfe07f752fa9da77a9904dc18
Riccardo Cannistrà <rickystream94@gmail.com>
2018-11-25 16:50:39 +0100

    Completed script to calculate centrality scores, almost completed hashtag analysis

--- scripts/hashtag_analysis/calculate_centrality.py
+++ scripts/hashtag_analysis/calculate_centrality.py
@@ -27,13 +27,49 @@ def calculate_betweenness_centrality(graph, hashtag):
             fout.write(",".join([str(edge.GetVal1()), str(edge.GetVal2()), str(Edges[edge])])+"\n")
     end = time.time()
     print("Completed in: %s" % timedelta(seconds=(int(end-start))))
+    
+def calculate_closeness_centrality(graph, hashtag):
+    start = time.time()
+    print("Calculating closeness centrality...")
+    closeness_centrality_scores = []
+    step = graph.GetNodes()/10
+    counter = 0
+    for node in graph.Nodes():
+        score = snap.GetClosenessCentr(graph, node.GetId())
+        if score == 0:
+            continue
+        closeness_centrality_scores.append((node.GetId(), score))
+        counter+=1
+        if counter % step == 0:
+            print("Progress: %d nodes" %counter)
+    print("Saving results to file...")
+    with open(hashtag+"_closeness_centrality.csv", "w") as fout:
+        fout.write("Node,Closeness Centrality\n")
+        for el in closeness_centrality_scores:
+            fout.write(",".join([str(el[0]), str(el[1])])+"\n")
+    end = time.time()
+    print("Completed in: %s" % timedelta(seconds=(int(end-start))))
+
+def calculate_eigenvector_centrality(graph, hashtag):
+    start = time.time()
+    print("Calculating eigenvector centrality...")
+    NIdEigenH = snap.TIntFltH()
+    snap.GetEigenVectorCentr(graph, NIdEigenH)
+    print("Saving results to file...")
+    with open(hashtag+"_eigenvector_centrality.csv", "w") as fout:
+        fout.write("Node,Eigenvector Centrality\n")
+        for item in NIdEigenH:
+            fout.write(",".join([str(item), str(NIdEigenH[item])])+"\n")
+    end = time.time()
+    print("Completed in: %s" % timedelta(seconds=(int(end-start))))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Must specify hashtag")
+    if len(sys.argv) != 3:
+        print("Must specify hashtag and centrality type (1==betwenness, 2==closeness, 3==eigenvector)")
         sys.exit(1)
     hashtag = sys.argv[1]
+    what = int(sys.argv[2])
 
     # Import the hashtag subgraph to work on
     FIn = snap.TFIn("../../data/mmr_subgraph_"+hashtag+".bin")
@@ -43,4 +79,9 @@ if __name__ == '__main__':
     hashtag_subgraph = snap.GetMxScc(hashtag_subgraph)
 
     # Start computation
-    calculate_betweenness_centrality(hashtag_subgraph, hashtag)
+    if what == 1:
+        calculate_betweenness_centrality(hashtag_subgraph, hashtag)
+    elif what == 2:
+        calculate_closeness_centrality(hashtag_subgraph, hashtag)
+    elif what == 3:
+        calculate_eigenvector_centrality(hashtag_subgraph, hashtag)
